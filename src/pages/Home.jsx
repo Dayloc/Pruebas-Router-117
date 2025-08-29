6967401c26fcbd3da11fc157a23a54ab5c664d03
Dayloc <158208881+Dayloc@users.noreply.github.com>
2025-08-29 08:12:20 +0000

    Cambios

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,48 +1,55 @@
 import rigoImageUrl from "../assets/img/rigo-baby.jpg";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
 import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 import { getUsers } from "../services/obtenerUsuarios.js";
 
 export const Home = () => {
+  const renders = useRef(0);
+  
+  const navigate = useNavigate();
 
-const navigate = useNavigate()
+  const [count, setCount] = useState(0);
 
-const {store, dispatch} =useGlobalReducer()
+  const { store, dispatch } = useGlobalReducer();
 
+  const { listaUsers } = store;
+  renders.current += 1;
 
-const {listaUsers} = store
+  useEffect(() => {
+    getUsers(dispatch);
+  }, []);
+  const obj = {
+    nombre: "Eduardo",
+    id: 1,
+    curso: 1999,
+  };
 
+  const navegacion = () => {
+    //navigate(`/prueba1/${obj["nombre"]}/${obj["id"]}`);
+    navigate(`/prueba1?nombre=${obj["nombre"]}&id=${obj["id"]}`);
+  };
 
-useEffect(() => {
+  console.log(listaUsers);
 
-	getUsers(dispatch)
+  return (
+    <div className="text-center mt-5">
+      <h1>Hello Rigo!!</h1>
+      <p>
+        <img src={rigoImageUrl} />
+      </p>
+      <button className="btn btn-primary" onClick={() => navegacion()}>
+        Ir Prueba1
+      </button>
 
-}, []);
+      {listaUsers &&
+        listaUsers.map((user) => <div key={user.id}>{user.name}</div>)}
 
-const nombre = "Eduardo";
-const id = 1
-
-const navegacion = ()=>{
-	navigate(`/prueba1/${nombre}/${id}`)
-}
-
-console.log(listaUsers)
-
-
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<button className="btn btn-primary" onClick={()=>navegacion()}>Ir Prueba1</button>
-
-			{
-				listaUsers && listaUsers.map((user)=>
-				<div key={user.id}>{user.name}</div>
-				)
-			}
-		</div>
-	);
-}; 
+      <div className="m-5">
+        <p>Contador: {count}</p>
+        <p>El componente se renderizó {renders.current} veces</p>
+        <button onClick={() => setCount(count + 1)}>+1</button>
+      </div>
+    </div>
+  );
+};
--- src/pages/Prueba1.jsx
+++ src/pages/Prueba1.jsx
@@ -1,15 +1,22 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React from "react";
+import { useParams, useLocation } from "react-router-dom";
 
 function Prueba1() {
-    const {id,nombre } = useParams()
-    
-    
+  //const { nombre, id } = useParams();
+  const location = useLocation();
+  const query = new URLSearchParams(location.search);
+
+    const nombre = query.get("nombre");
+  const id = query.get("id");
   return (
     <div>
       Hola, soy Prueba 1 y me ha llegado por url {nombre} y {id}
+      <div>
+        <p>Ruta actual: {location.pathname}</p>
+        <p>Query params: {location.search}</p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Prueba1
+export default Prueba1;
